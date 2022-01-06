f2724c5c5876fa2f79708597a52a3af99c40b28a
Cyril Florval <ascread@gmail.com>
2022-01-06 13:39:58 +0100

    Save mal organisé

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+const http = require("http")
 const express = require("express")
-const app = express()
-require ("./app")
+const app = require("./app")
+
+const server = http.createServer(app)
 
 app.listen(3000, () => console.log("server started 3000"))

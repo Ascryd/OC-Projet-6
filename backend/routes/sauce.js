73cb92c3672a7c5627c152851bf0fd3e7ada834c
Cyril Florval <ascread@gmail.com>
2022-01-12 16:41:03 +0100

    Routes sécurisés

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,12 +2,14 @@ const express = require("express")
 const router = express.Router()
 const multer = require("../middleware/multer-config")
 const sauceCtrl = require ("../controllers/sauce")
+const auth = require("../middleware/auth")
 
 
-router.post("/", multer, sauceCtrl.addSauce)
-router.put("/:id", multer, sauceCtrl.modifySauce)
-router.delete("/:id", sauceCtrl.deleteOne)
-router.get("/:id", sauceCtrl.getOneSauce)
-router.get("/", sauceCtrl.getAllSauces)
+router.post("/", auth, multer, sauceCtrl.addSauce)
+router.put("/:id", auth, multer, sauceCtrl.modifySauce)
+router.delete("/:id", auth, sauceCtrl.deleteOne)
+router.get("/:id", auth, sauceCtrl.getOneSauce)
+router.get("/", auth, sauceCtrl.getAllSauces)
+router.post("/:id/like", auth, sauceCtrl.like)
 
 module.exports = router
